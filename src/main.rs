079b4b6d3d62544f15ba17edb519ab039c20a910
Daniel McCrevan <danielmccrevan@gmail.com>
2020-05-04 14:47:34 -0400

    Added basic build message function and test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+mod proto;
+use proto::msg::Message;
+
 fn main() {
-    println!("Hello, world!");
+    let name = String::from("test");
+    let msg = build_message(name);
+    println!("{:?}", msg);
+}
+
+fn build_message(name: String) -> Message {
+    let mut msg = Message::new();
+    msg.set_body(name);
+    msg
+}
+
+#[test]
+fn build_message_test() {
+    let name = String::from("test");
+    let msg = build_message(name.clone());
+    assert_eq!(msg.get_body(), name);
 }
